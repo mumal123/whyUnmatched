4e7389505dea9eedcaf97ed594420dc8a120779c
mumal123 <mumalswami@gmail.com>
2026-08-29 15:49:28 +0530

    Added Synthetic payments dataset

---
+++ data/payments.py
@@ -0,0 +1,20 @@
+from faker import Faker
+import random
+import pandas as pd
+
+f=Faker(["en_IN"])
+num_transactions = 1000
+random.seed(42)
+Faker.seed(42)
+
+
+payments=[]
+for _ in range(num_transactions):
+    payment={}
+    payment["payment_id"] = f.uuid4()
+    payment["order_id"] = f.uuid4()
+    payment["payment_amount"] = f.random_int(min=10, max=10000000)
+    payment['time'] = f.date_time_this_year()
+    payments.append(payment)
+
+pd.DataFrame(payments).to_csv("payments.csv", mode="a", index=False, header=True)
